cbd294add499d94230c42365ec0b7fe3c8f535e8
Agustín Mario Vicchio <agustinvicchio@fb.com>
2021-04-01 11:41:05 -0300

    agus creo repo, carpetas y archivos

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express ();
+const port = 3000;
+const path = require('path');
+app.use (express.static('public'));
+
+
+app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/index.html'));});
+app.listen (3000, () => { console.log('servidor corriendo')});
