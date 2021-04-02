51fbee3feb026c52292dc3f892a357ca2c1d685b
Marc319-jr <marcelo.sznek@gmail.com>
2021-04-02 11:08:31 -0300

    fotos orfen y index de celus

--- app.js
+++ app.js
@@ -6,4 +6,6 @@ app.use (express.static('public'));
 
 
 app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/index.html'));});
+
+
 app.listen (3000, () => { console.log('servidor corriendo')});
