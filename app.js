6ec6ff485b8e6ab6d89e43ed074fdff44fb9d339
Agustín Mario Vicchio <agustinvicchio@fb.com>
2021-04-08 18:54:57 -0300

    agustin hizo un add para sincronizar archivos

--- app.js
+++ app.js
@@ -5,5 +5,5 @@ const path = require('path');
 app.use (express.static('public'));
 
 
-app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/index.html'));});
+app.get ('/', (req,res) => { res.sendFile (path.join(__dirname,'/views/product.html'));});
 app.listen (3000, () => { console.log('servidor corriendo')});
